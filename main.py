1630e7682c9b76a466647415dca2400e3267c9fd
Olivia <ohowse@ufl.edu>
2023-07-18 23:01:44 -0400

    lab 6 encoder and decoder

---
+++ main.py
@@ -0,0 +1,60 @@
+def decoder(password):
+    decoded_password = []
+    for i in range(len(password)):
+        # change to int type
+        integer = int(password[i])
+        # add 3
+        integer -= 3
+        # append to a new list
+        decoded_password.append(integer)
+    return decoded_password
+
+
+def encoder(password):
+    encoded_password = []
+    # get each # separately
+    for i in range(len(password)):
+        # change to int type
+        integer = int(password[i])
+        # add 3
+        integer += 3
+        # append to a new list
+        encoded_password.append(integer)
+    return encoded_password
+
+
+def print_list(array):
+    for i in array:
+        print(i, end='')
+
+
+if __name__ == '__main__':
+    # menu
+    while True:
+        # print menu
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+
+        # get option
+        user_input = int(input('\nPlease enter an option: '))
+
+        # if statements for option
+        if user_input == 1:
+            print('Please enter your password to encode:', end=' ')
+            # store password
+            user_password = input()
+            print('Your password has been encoded and stored!')
+        elif user_input == 2:
+            print('The encoded password is', end=' ')
+            # encode password
+            new_password = encoder(user_password)
+            print_list(new_password)
+            print(', and the original password is ', end='')
+            old_password = decoder(new_password)
+            print_list(old_password)
+            print('.')
+        elif user_input == 3:
+            break
